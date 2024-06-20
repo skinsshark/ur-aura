96aa73b4b55957b1c4f126aea557f2b7a5ab2231
Sharon Zheng <sharzheng@gmail.com>
2024-06-19 18:26:35 -0700

    pass in ref for auras

--- src/Aura.tsx
+++ src/Aura.tsx
@@ -1,53 +1,58 @@
-import { useEffect, useRef } from 'react';
+import { forwardRef, useEffect } from 'react';
 import { FacePositionType } from './Camera';
 
-function Aura({
-  d,
-  fillColor,
-  svgWidth,
-  svgHeight,
-  facePosition,
-  width,
-  height,
-  top,
-  left,
-  fillOpacity = 0.9,
-}: {
-  d: string;
-  fillColor: string;
+type SvgComponentProps = {
   svgWidth: number;
   svgHeight: number;
+  d: string;
+  fillColor: string;
   facePosition: FacePositionType;
   width: number;
   height: number;
   top: number;
   left: number;
   fillOpacity?: number;
-}) {
-  const auraRef = useRef<SVGSVGElement | null>(null);
+};
 
-  useEffect(() => {
-    if (auraRef.current) {
-      auraRef.current.setAttribute('width', `${Math.round(width)}%`);
-      auraRef.current.setAttribute('height', `${Math.round(height)}%`);
-      auraRef.current.style.top = `${Math.round(top - 10)}%`;
-      auraRef.current.style.left = `${Math.round(left)}%`;
-    }
-  }, [facePosition, height, auraRef, width, top, left]);
+const Aura = forwardRef<SVGSVGElement, SvgComponentProps>(
+  (
+    {
+      d,
+      fillColor,
+      svgWidth,
+      svgHeight,
+      facePosition,
+      width,
+      height,
+      top,
+      left,
+      fillOpacity = 0.9,
+    },
+    ref
+  ) => {
+    useEffect(() => {
+      if (ref && typeof ref !== 'function' && ref.current) {
+        ref.current.setAttribute('width', `${Math.round(width)}%`);
+        ref.current.setAttribute('height', `${Math.round(height)}%`);
+        ref.current.style.top = `${Math.round(top - 10)}%`;
+        ref.current.style.left = `${Math.round(left)}%`;
+      }
+    }, [facePosition, height, ref, width, top, left]);
 
-  return (
-    <svg
-      ref={auraRef}
-      width={svgWidth}
-      height={svgHeight}
-      viewBox={`0 0 ${svgWidth} ${svgHeight}`}
-      fill="none"
-      fillOpacity={fillOpacity}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d={d} fill={fillColor} />
-    </svg>
-  );
-}
+    return (
+      <svg
+        ref={ref}
+        width={svgWidth}
+        height={svgHeight}
+        viewBox={`0 0 ${svgWidth} ${svgHeight}`}
+        fill="none"
+        fillOpacity={fillOpacity}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d={d} fill={fillColor} />
+      </svg>
+    );
+  }
+);
 
 export default Aura;
