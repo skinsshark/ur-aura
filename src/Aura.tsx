e5578e4f879aa2a66023bcf3828b37e55db39849
Sharon Zheng <sharzheng@gmail.com>
2024-07-08 19:45:37 -0700

    fix safari filtering ie rm AuraFilter

--- src/Aura.tsx
+++ src/Aura.tsx
@@ -1,5 +1,4 @@
 import { forwardRef, useEffect } from 'react';
-import AuraFilter from './AuraFilter';
 import { FacePositionType } from './CapturedImage';
 
 type AuraProps = {
@@ -53,7 +52,25 @@ const Aura = forwardRef<SVGSVGElement, AuraProps>(
         preserveAspectRatio="xMidYMid meet"
       >
         <defs>
-          <AuraFilter />
+          <filter id="wavy">
+            <feTurbulence
+              id="turbulence"
+              type="turbulence"
+              numOctaves="10"
+              result="NOISE"
+            ></feTurbulence>
+            <feDisplacementMap
+              in="SourceGraphic"
+              in2="NOISE"
+              scale="30"
+            ></feDisplacementMap>
+            <animate
+              xlinkHref="#turbulence"
+              attributeName="baseFrequency"
+              values="0.01 0;0.02 0;0.01 0"
+              repeatCount="indefinite"
+            ></animate>
+          </filter>
         </defs>
         <path d={d} fill={fillColor} />
       </svg>
