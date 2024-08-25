a1bba030d9410d23f141633671a8b3ca06bc1c76
Sharon Zheng <sharzheng@gmail.com>
2024-08-24 23:02:30 -0700

    rm comments

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,7 +1,6 @@
 import ReactGA from 'react-ga4';
 
 export const trackCustomEvent = (eventName: string) => {
-  console.log('asdf');
   ReactGA.event({
     category: 'user-action',
     action: eventName,
